5b7ab916a7011b6f1d04324406365f67c8bfe724
vademich <goncharov1999vadim@gmail.com>
2025-06-10 14:07:54 +0300

    typo

--- src/app/routes-table/routes-table.component.ts
+++ src/app/routes-table/routes-table.component.ts
@@ -17,7 +17,7 @@ interface RouteTableItem {
 export class RoutesTableComponent implements OnInit {
     private routes: Route[] = [
         { uuid: '1', address: '0.0.0.0', mask: '0', gateway: '193.0.174.1', interface: 'Подключение Ethernet' },
-        { uuid: '2', address: '10.1.30.0', mask: '24', gateway: '0.0.0.0', interface: 'Гостовая сеть' },
+        { uuid: '2', address: '10.1.30.0', mask: '24', gateway: '0.0.0.0', interface: 'Гостевая сеть' },
         { uuid: '3', address: '192.168.1.0', mask: '24', gateway: '0.0.0.0', interface: 'Домашняя сеть' },
         { uuid: '4', address: '193.0.174.0', mask: '24', gateway: '0.0.0.0', interface: 'Подключение Ethernet' },
         { uuid: '5', address: '193.0.175.0', mask: '25', gateway: '193.0.174.10', interface: 'Подключение Ethernet' },
